ab7d902dcfc81d9614bfbab4d4d22e1e87299111
Krzysztof Wójcik <krzysztofw.2744@gmail.com>
2021-06-07 02:38:36 +0200

    Add libraries required for testing the app Play with ExampleUnitTest

--- app/src/test/java/pl/makrohard/alfacommerce/ExampleUnitTest.kt
+++ app/src/test/java/pl/makrohard/alfacommerce/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package pl.makrohard.alfacommerce
 
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import pl.makrohard.alfacommerce.domain.model.Category
+import pl.makrohard.alfacommerce.domain.repository.CategoriesRepository
+import kotlin.test.assertTrue
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +15,25 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private lateinit var categoriesRepository: CategoriesRepository
+
+    @Before
+    fun setup() {
+        val list = listOf(
+            Category(1, "Hoodies"),
+            Category(2, "T-shirts"),
+            Category(3, "Caps"),
+            Category(4, "Sneakers"),
+        )
+
+        categoriesRepository = mock {
+            on { runBlocking { index() } } doReturn list
+        }
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun repository_getData() = runBlocking {
+        val categoriesList = categoriesRepository.index()
+        assertTrue(true, "xd")
     }
 }
